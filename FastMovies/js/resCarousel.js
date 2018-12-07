64b882d48915b8bee3742159324cdf1c5a6e98aa
Carol Ataíde <30929945+sknyx@users.noreply.github.com>
2018-12-07 00:05:14 -0200

    +atualizações

--- FastMovies/js/resCarousel.js
+++ FastMovies/js/resCarousel.js
@@ -326,3 +326,37 @@ $(document).ready(function() {
 });
 
 //NARUTOOOOOOOOOOOOOOOOOOOOO
+//SUSUKEEEEEEEEEEEEEEEEEEEEE
+
+/* JS pro menu de navegação principal */
+let mainNavLinks = document.querySelectorAll("#navMain ul li a");
+let mainSections = document.querySelectorAll(".container");
+
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
